ed73e6c1da9100ae7d7303fee62085fb36dddd12
lucasew <lucas59356@gmail.com>
2021-05-21 17:15:06 -0300

    tentando outra abordagem, acho que o banco tava certo
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/main/java/io/tictactoe/Main.java
+++ src/main/java/io/tictactoe/Main.java
@@ -1,18 +1,23 @@
 package io.tictactoe;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.Startup;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import io.tictactoe.controller.db.HistoricoController;
 import io.tictactoe.utils.AppLogger;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Properties;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 @QuarkusMain
 public class Main {
     public static void main(String[] args) {
-//        LogManager.getLogManager().reset();
-//        Logger rootLogger = LogManager.getLogManager().getLogger("");
-//        rootLogger.addHandler(new AppLogger());
         Quarkus.run(args);
     }
 }
