76315d323ba5b8ca3e1b574a606a90d3bbf337e6
lucasew <lucas59356@gmail.com>
2025-05-14 14:00:21 -0300

    coisas pra deployar no fly
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     openjdk11
     maven
-    idea.idea-ultimate # i have a license from uni
+    # idea.idea-ultimate # i have a license from uni
   ];
   shellHook = ''
   function run_dev {
